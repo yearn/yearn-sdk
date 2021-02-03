b8c9d212b7e6ed76a4427c55411d54ede0fc3365
x48 <x48114@protonmail.com>
2021-02-02 22:54:38 -0600

    refactor: remove unnecessary logging

--- test/data/assets.test.ts
+++ test/data/assets.test.ts
@@ -3,8 +3,6 @@ import { fetchAliases, fetchAssets } from "../../src/data/assets";
 describe("assets", () => {
   it("fetch aliases (network)", async () => {
     const aliases = await fetchAliases();
-    // eslint-disable-next-line no-console
-    console.error(aliases);
     return expect(Object.values(aliases)).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
@@ -17,8 +15,6 @@ describe("assets", () => {
   });
   it("fetch assets (network)", async () => {
     const assets = await fetchAssets();
-    // eslint-disable-next-line no-console
-    console.error(assets);
     return expect(Object.values(assets)).toEqual(
       expect.arrayContaining([expect.any(String)])
     );
