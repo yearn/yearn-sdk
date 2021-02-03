b95fc683c3444d1541cfd3dddb367d987bfdd52b
x48 <x48114@protonmail.com>
2021-02-02 22:55:02 -0600

    refactor: only fetch the first 3 transactions when fetching inception block

--- src/vault/reader/common.ts
+++ src/vault/reader/common.ts
@@ -7,7 +7,10 @@ export async function fetchInceptionBlock(
   vault: Vault,
   ctx: Context
 ): Promise<TimedBlock | null> {
-  const txlist = await fetchTransactionList({ address: vault.address }, ctx);
+  const txlist = await fetchTransactionList(
+    { address: vault.address, page: 1, offset: 3 },
+    ctx
+  );
   if (txlist.length < 3) return null;
   const inception = txlist[2]; // skip contract creation
   return { block: inception.blockNumber, timestamp: inception.timeStamp };
