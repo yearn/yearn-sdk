c2ce7e6b4b05c02efa92c4ff19dc745fbb5afda4
nymmrx <nymmrx@protonmail.com>
2021-03-23 17:27:34 +0100

    chore: add readme

--- examples/oracle.js
+++ examples/oracle.js
@@ -8,7 +8,10 @@ const dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 
 async function main() {
   const price = await yearn.oracle.getPriceUsdcRecommended(dai, provider);
-  console.log(`Dai price is ${price} / 1e6 USDC`)
+  console.log(`Dai price is ${price} / 1e6 USDC`);
+
+  const markets = await yearn.oracle.getIronBankMarkets(provider);
+  console.log(markets)
 }
 
 main();
