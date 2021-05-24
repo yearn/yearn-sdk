869c259f852344987fb0a4fdc0dfdf3a21126460
nymmrx <nymmrx@protonmail.com>
2021-05-24 11:38:41 +0200

    ci: refactor workflows (#24)
    * ci: refactor workflows
    
    * ci: rename jobs
    
    * ci: rename file

--- examples/common.mjs
+++ examples/common.mjs
@@ -2,8 +2,8 @@ export const YearnGovernance = "0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52";
 export const Addresses = !process.env.WEB3_PROVIDER && {
   adapters: {
     registryV2: "0xFbD588c72B438faD4Cf7cD879c8F730Faa213Da0",
-    ironBank: "0xed00238F9A0F7b4d93842033cdF56cCB32C781c2"
+    ironBank: "0xed00238F9A0F7b4d93842033cdF56cCB32C781c2",
   },
   helper: "0x420b1099B9eF5baba6D92029594eF45E19A04A4A",
-  oracle: "0xE7eD6747FaC5360f88a2EFC03E00d25789F69291"
+  oracle: "0xE7eD6747FaC5360f88a2EFC03E00d25789F69291",
 };
