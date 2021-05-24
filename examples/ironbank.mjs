712ebafd09540cc05e0e8e3e506429ae021e293e
nymmrx <nymmrx@protonmail.com>
2021-05-24 22:03:34 +0200

    fix(zapper): support latest API changes (#27)

--- examples/ironbank.mjs
+++ examples/ironbank.mjs
@@ -1,23 +1,22 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
 import Table from "cli-table3";
 
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { YearnGovernance, Addresses } from "./common.mjs";
-
 import { Yearn } from "../dist/index.js";
+import { Addresses, YearnGovernance } from "./common.mjs";
 
 const url = process.env.WEB3_PROVIDER || "http://localhost:8545";
 const provider = new JsonRpcProvider(url);
 
 const yearn = new Yearn(1, {
   provider,
-  addresses: Addresses,
+  addresses: Addresses
 });
 
 async function main() {
   const ironBank = await yearn.ironBank.get();
 
   const ironBankTable = new Table();
-  ironBankTable.push(...ironBank.map((market) => [market.name, market.address, market.typeId]));
+  ironBankTable.push(...ironBank.map(market => [market.name, market.address, market.typeId]));
 
   console.log("IronBank markets:");
   console.log(ironBankTable.toString());
@@ -32,7 +31,7 @@ async function main() {
   const ironBankUserMetadataTable = new Table();
   const ironBankUserMetadata = await yearn.ironBank.userMetadata(YearnGovernance);
 
-  ironBankUserMetadataTable.push(...ironBankUserMetadata.map((market) => Object.values(market)));
+  ironBankUserMetadataTable.push(...ironBankUserMetadata.map(market => Object.values(market)));
   console.log("Yearn Multisig IronBank user metadata:");
   console.log(ironBankUserMetadataTable.toString());
 }
