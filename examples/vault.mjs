2067d04bf509d34ec37e0c64c2f889dee22506e5
nymmrx <nymmrx@protonmail.com>
2021-05-24 22:03:34 +0200

    fix(zapper): support latest API changes (#27)

--- examples/vault.mjs
+++ examples/vault.mjs
@@ -1,23 +1,22 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
 import Table from "cli-table3";
 
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { YearnGovernance, Addresses } from "./common.mjs";
-
 import { Yearn } from "../dist/index.js";
+import { Addresses, YearnGovernance } from "./common.mjs";
 
 const url = process.env.WEB3_PROVIDER || "http://localhost:8545";
 const provider = new JsonRpcProvider(url);
 
 const yearn = new Yearn(1, {
   provider,
-  addresses: Addresses,
+  addresses: Addresses
 });
 
 async function main() {
   const vaults = await yearn.vaults.get();
 
   const vaultsTable = new Table();
-  vaultsTable.push(...vaults.map((vault) => [vault.name, vault.address, vault.typeId]));
+  vaultsTable.push(...vaults.map(vault => [vault.name, vault.address, vault.typeId]));
 
   console.log("V1 & V2 vaults:");
   console.log(vaultsTable.toString());
@@ -26,8 +25,8 @@ async function main() {
 
   const positionsTable = new Table();
   positionsTable.push(
-    ...positions.map((position) => {
-      const vault = vaults.find((vault) => (vault.address = position.assetAddress));
+    ...positions.map(position => {
+      const vault = vaults.find(vault => (vault.address = position.assetAddress));
       return [vault.name, position.balance];
     })
   );
