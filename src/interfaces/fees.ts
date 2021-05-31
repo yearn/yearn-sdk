bcc2e95735071b2bf6ccfcf7e84d1a017ded227b
Tom Pearson <tom.pearson@team.bumble.com>
2021-05-31 15:34:44 +0100

    fix: linted

--- src/interfaces/fees.ts
+++ src/interfaces/fees.ts
@@ -1,12 +1,13 @@
+import BigNumber from "bignumber.js";
+
 import { ChainId } from "../chain";
 import { ServiceInterface } from "../common";
-import { Usdc } from "../types";
 import {
   ProtocolFees as ProtocolFeesQuery,
   ProtocolFeesVariables as ProtocolFeesQueryVariables
 } from "../services/subgraph/apollo/generated/ProtocolFees";
 import { PROTOCOL_FEES } from "../services/subgraph/apollo/queries";
-import BigNumber from "bignumber.js";
+import { Usdc } from "../types";
 
 export class FeesInterface<C extends ChainId> extends ServiceInterface<C> {
   async protocolFees(since: Date): Promise<Usdc> {
