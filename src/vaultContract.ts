d81d8f69bde31dabade49cf6f3e9911abb3a4d24
JStash <jstashh@protonmail.com>
2021-07-15 20:48:27 +0100

    fix: added vaultContract file

---
+++ src/vaultContract.ts
@@ -0,0 +1,67 @@
+import { Contract } from "@ethersproject/contracts";
+import { JsonRpcSigner } from "@ethersproject/providers";
+import { BigNumber } from "bignumber.js";
+
+import { Address } from "./types/common";
+
+export abstract class VaultContract {
+  protected static get abi() {
+    return [
+      "function token() view returns (address)",
+      "function decimals() view returns (uint256)",
+      "function deposit(uint256 amount) public",
+      "function withdraw(uint256 amount) public"
+    ];
+  }
+
+  constructor(protected contract: Contract) {}
+
+  abstract pricePerShare(): Promise<BigNumber>;
+
+  async decimals(): Promise<BigNumber> {
+    const decimals = await this.contract.decimals();
+    return new BigNumber(decimals.toString());
+  }
+
+  async token(): Promise<Address> {
+    return await this.contract.token();
+  }
+
+  encodeDeposit = (amount: string): string => {
+    return this.contract.interface.encodeFunctionData("deposit", [amount]);
+  };
+
+  encodeWithdraw = (amount: string): string => {
+    return this.contract.interface.encodeFunctionData("withdraw", [amount]);
+  };
+}
+
+export class PickleJarContract extends VaultContract {
+  private static get pickleJarAbi() {
+    return ["function getRatio() public view returns (uint256)"];
+  }
+
+  constructor(jarAddress: Address, signer: JsonRpcSigner) {
+    super(new Contract(jarAddress, [...VaultContract.abi, ...PickleJarContract.pickleJarAbi], signer));
+  }
+
+  async pricePerShare(): Promise<BigNumber> {
+    const ratio = await this.contract.getRatio();
+    return new BigNumber(ratio.toString());
+  }
+}
+
+export class YearnVaultContract extends VaultContract {
+  private static get yearnVaultAbi() {
+    return ["function pricePerShare() view returns (uint256)"];
+  }
+
+  constructor(jarAddress: Address, signer: JsonRpcSigner) {
+    super(new Contract(jarAddress, [...VaultContract.abi, ...YearnVaultContract.yearnVaultAbi], signer));
+  }
+
+  async pricePerShare(): Promise<BigNumber> {
+    const pricePerShare = await this.contract.pricePerShare();
+    return new BigNumber(pricePerShare.toString());
+  }
+}
