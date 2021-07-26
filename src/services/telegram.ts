e60919e8115d0dd683573d232b5e72e104b2a6a3
jstashh <jstashh@protonmail.com>
2021-07-26 20:14:36 +0100

    feat(simulations): added anomaly detection via telegram message [WEB-612] (#78)
    * feat(simulations): added anomoly detection via telegram message
    
    * fix: linted
    
    * fix: typo

---
+++ src/services/telegram.ts
@@ -0,0 +1,26 @@
+import { Service } from "../common";
+import { SdkError } from "../types";
+
+export class TelegramService extends Service {
+  private get chatId(): string {
+    const chatId = process.env.TELEGRAM_CHAT_ID;
+    if (!chatId) {
+      throw new SdkError("Telegram Chat Id not set");
+    }
+    return chatId;
+  }
+
+  private get botId(): string {
+    const botId = process.env.TELEGRAM_BOT_ID;
+    if (!botId) {
+      throw new SdkError("Telegram Bot Id not set");
+    }
+    return botId;
+  }
+
+  sendMessage(text: string) {
+    const params = new URLSearchParams({ chat_id: this.chatId, text: text });
+    const url = `https://api.telegram.org/bot${this.botId}/sendMessage?` + params;
+    fetch(url);
+  }
+}
