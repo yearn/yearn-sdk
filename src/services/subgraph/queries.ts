eb9eb20730fbed19b449cd2deb4b616a777bc69d
JStash <19509999+jstashh@users.noreply.github.com>
2021-08-09 14:54:11 +0100

    chore: removed codegen script and linted

---
+++ src/services/subgraph/queries.ts
@@ -0,0 +1,103 @@
+export const VAULT_EARNINGS = `query VaultEarnings($vault: ID!) {
+    vault(id: $vault) {
+      token {
+        id
+        decimals
+      }
+      latestUpdate {
+        returnsGenerated
+      }
+    }
+  }
+`;
+
+export const PROTOCOL_EARNINGS = `query ProtocolEarnings {
+    vaults {
+      token {
+        id
+        decimals
+      }
+      latestUpdate {
+        returnsGenerated
+      }
+    }
+  }
+`;
+
+export const ACCOUNT_EARNINGS = `query AccountEarnings($id: ID!) {
+    account(id: $id) {
+      vaultPositions {
+        balanceShares
+        token {
+          id
+          decimals
+        }
+        shareToken {
+          symbol
+        }
+        updates {
+          deposits
+          withdrawals
+          tokensReceived
+          tokensSent
+        }
+        vault {
+          id
+          latestUpdate {
+            pricePerShare
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const ASSET_HISTORIC_EARNINGS = `query AssetHistoricEarnings($id: ID!, $sinceDate: BigInt!) {
+    vault(id: $id) {
+      id
+      token {
+        id
+        decimals
+      }
+      vaultDayData(where: { timestamp_gte: $sinceDate }, first: 1000) {
+        dayReturnsGenerated
+        timestamp
+      }
+    }
+  }
+`;
+
+export const ACCOUNT_HISTORIC_EARNINGS = `query AccountHistoricEarnings($id: ID!, $shareToken: String!, $sinceDate: BigInt!) {
+    account(id: $id) {
+      vaultPositions(where: { shareToken: $shareToken }) {
+        balanceShares
+        token {
+          id
+          decimals
+        }
+        vault {
+          vaultDayData(where: { timestamp_gte: $sinceDate }, orderBy: timestamp, orderDirection: asc, first: 1000) {
+            pricePerShare
+            timestamp
+            tokenPriceUSDC
+          }
+        }
+        updates(orderBy: timestamp, orderDirection: asc, first: 1000) {
+          balanceShares
+          timestamp
+          deposits
+          withdrawals
+          tokensReceived
+          tokensSent
+        }
+      }
+    }
+  }
+`;
+
+export const PROTOCOL_FEES = `query ProtocolFees($sinceDate: BigInt!) {
+    transfers(where: { timestamp_gt: $sinceDate }, first: 1000) {
+      tokenAmountUsdc
+    }
+  }
+`;
