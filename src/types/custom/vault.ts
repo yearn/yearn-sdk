72ab5137d5cd3c416ed8f92f7939e1bdd33f25c1
jstashh <19509999+jstashh@users.noreply.github.com>
2021-08-16 12:33:24 +0100

    fix: removed reliance on old vaults.finance api [WEB-694] (#87)
    * fix: removed reliance on old vaults finance api
    
    * fix: used cloudfront url

--- src/types/custom/vault.ts
+++ src/types/custom/vault.ts
@@ -5,11 +5,29 @@ import { ZapOptions } from "./zapper";
  * Annual Percentage Yield of a particular vault.
  */
 export interface Apy {
-  recommended: number;
-  composite: boolean;
   type: string;
-  description: string;
-  data?: Record<string, unknown>;
+  gross_apr: number;
+  net_apy: number;
+  fees: {
+    performance: number | null;
+    withdrawal: number | null;
+    management: number | null;
+    keep_crv: number | null;
+    cvx_keep_crv: number | null;
+  };
+  points: {
+    week_ago: number;
+    month_ago: number;
+    inception: number;
+  } | null;
+  composite: {
+    boost: number;
+    pool_apy: number;
+    boosted_apr: number;
+    base_apr: number;
+    cvx_apr: number;
+    rewards_apr: number;
+  } | null;
 }
 
 export type ApyMap<T extends Address> = TypedMap<T, Apy | undefined>;
