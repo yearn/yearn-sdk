2cd5420cf8f2378beae1f1f77ad683f85a3502e1
jstash <19509999+jstashh@users.noreply.github.com>
2021-08-24 13:24:54 +0100

    fix: made cache url optional

--- src/context.ts
+++ src/context.ts
@@ -41,7 +41,7 @@ export interface TelegramConfiguration {
 
 export interface CacheConfiguration {
   useCache: boolean;
-  url: string;
+  url?: string;
 }
 
 /**
--- src/services/cache.ts
+++ src/services/cache.ts
@@ -2,7 +2,7 @@ import { Service } from "../common";
 
 export class CacheService extends Service {
   async call(path: string): Promise<unknown> {
-    if (!this.ctx.cache.useCache) {
+    if (!this.ctx.cache.useCache || !this.ctx.cache.url) {
       return undefined;
     }
     const cachedCall = await fetch(`${this.ctx.cache.url}/v1/chains/${this.chainId}/${path}`);
