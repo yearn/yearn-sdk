e3fbb50f4277be5e7bcbe6c05ff9c4b04b13c84c
jstashh <19509999+jstashh@users.noreply.github.com>
2021-09-10 15:25:15 +0100

    fix: added pagination for assetsDynamic (#112)

--- src/helpers.ts
+++ src/helpers.ts
@@ -24,3 +24,12 @@ export function usdc(usd: any): Usdc {
 export function int(value: BigNumber): Integer {
   return value.toString();
 }
+
+export function chunkArray<T>(array: T[], size: number) {
+  let result = [];
+  for (let i = 0; i < array.length; i += size) {
+    let chunk = array.slice(i, i + size);
+    result.push(chunk);
+  }
+  return result;
+}
