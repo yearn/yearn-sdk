3ec1f0f5ee8ffa1be73907aa6db8b5afc3afd760
jstash <19509999+jstashh@users.noreply.github.com>
2021-09-16 12:09:34 +0100

    fix: removed unnecessary url

--- examples/fantom/vault.mjs
+++ examples/fantom/vault.mjs
@@ -3,7 +3,7 @@ import { JsonRpcProvider } from "@ethersproject/providers";
 import { Yearn } from "../../dist/index.js";
 import { Addresses } from "../common.mjs";
 
-const url = "https://rpc.ftm.tools/" || "http://localhost:8545";
+const url = "https://rpc.ftm.tools/";
 const provider = new JsonRpcProvider(url);
 
 const yearn = new Yearn(250, {
