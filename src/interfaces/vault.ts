969535d3c746ad5cbb8aa7d004c5b08e38cdcdb5
jstashh <19509999+jstashh@users.noreply.github.com>
2021-09-22 19:21:08 +0100

    feat: implemented apy override (#125)
    * feat: implemented apy override
    
    * feat: added chain settings metadata (#124)
    
    * feat: added chain settings metadata
    
    * fix: snake case to camel case
    
    * fix: added apyTypeOverride property

--- src/interfaces/vault.ts
+++ src/interfaces/vault.ts
@@ -10,6 +10,7 @@ import { chunkArray, EthAddress, WethAddress } from "../helpers";
 import { PickleJars } from "../services/partners/pickle";
 import {
   Address,
+  Apy,
   Balance,
   DepositOptions,
   Integer,
@@ -106,6 +107,18 @@ export class VaultInterface<T extends ChainId> extends ServiceInterface<T> {
       dynamic.metadata.withdrawalsDisabled = overrides?.withdrawalsDisabled;
       dynamic.metadata.allowZapIn = overrides?.allowZapIn;
       dynamic.metadata.allowZapOut = overrides?.allowZapOut;
+      if (overrides?.apyOverride) {
+        if (!dynamic.metadata.apy) {
+          dynamic.metadata.apy = this.makeEmptyApy();
+        }
+        dynamic.metadata.apy.net_apy = overrides.apyOverride;
+      }
+      if (overrides?.apyTypeOverride) {
+        if (!dynamic.metadata.apy) {
+          dynamic.metadata.apy = this.makeEmptyApy();
+        }
+        dynamic.metadata.apy.type = overrides.apyTypeOverride;
+      }
 
       assetsWithMetadataOverrides.push({ vault: { ...asset, ...dynamic }, overrides });
     }
@@ -444,5 +457,17 @@ export class VaultInterface<T extends ChainId> extends ServiceInterface<T> {
 
       throw error;
     }
+  }
+
+  private makeEmptyApy(): Apy {
+    const apy: Apy = {
+      type: "manual_override",
+      gross_apr: 0,
+      net_apy: 0,
+      fees: { performance: null, withdrawal: null, management: null, keep_crv: null, cvx_keep_crv: null },
+      points: null,
+      composite: null
+    };
+    return apy;
   }
 }
