01fed9432850d17ab7755f9208fc11168e1261c1
jstash <19509999+jstashh@users.noreply.github.com>
2021-09-25 19:26:11 +0100

    fix: fetch iron bank tokens for fantom balances

--- examples/fantom/vault.mjs
+++ examples/fantom/vault.mjs
@@ -16,9 +16,9 @@ async function main() {
   const vaults = await yearn.vaults.getDynamic();
   console.log(vaults);
   const tokenMetadata = await yearn.services.meta.token("0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83");
-  console.log(result);
+  console.log(tokenMetadata);
   const vaultMetadata = await yearn.services.meta.vaults("0x1DF3f4985Ac2103B96ed75f9d76F33696D2b1d66");
-  console.log(result);
+  console.log(vaultMetadata);
 }
 
 main();
