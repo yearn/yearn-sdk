556d4f98854a6f744a4a963e589a3f66889dc59b
jstash <19509999+jstashh@users.noreply.github.com>
2021-10-14 14:35:37 +0100

    fix: update adapter address again

--- src/services/adapters/ironbank.ts
+++ src/services/adapters/ironbank.ts
@@ -66,7 +66,7 @@ export class IronBankAdapter<T extends ChainId> extends ContractService<T> {
       case 1337:
         return "0xFF0bd2d0C7E9424ccB149ED3757155eEf41a793D";
       case 250:
-        return "0x1164587b49ff7aCE303962cC7A7E0841C1b34986";
+        return "0x8CafAF31Ee6374C02EedF1AD68dDb193dDAC29A2";
     }
   }
 
