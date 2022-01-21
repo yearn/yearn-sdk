58bf7c55e797fbce3a55b4748d96f7eac806f764
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-01-21 00:06:05 -0600

    chore: remove log

--- src/interfaces/vault.ts
+++ src/interfaces/vault.ts
@@ -205,7 +205,6 @@ export class VaultInterface<T extends ChainId> extends ServiceInterface<T> {
               assets.map(asset => asset.address)
             );
           }
-          console.log("Chunk 1");
           const chunks = chunkArray(allAddresses, 1);
           const promises = chunks.map(async chunk => adapter.positionsOf(address, chunk, overrides));
           return await Promise.all(promises).then(chunks => chunks.flat());
