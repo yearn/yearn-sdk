ef4d0812cbca37d06d0c36f1f07d8ad94059215c
jstash <19509999+jstashh@users.noreply.github.com>
2022-02-02 16:20:22 +0000

    fix: add babel config

--- src/validatedSigner.ts
+++ src/validatedSigner.ts
@@ -1,33 +1,39 @@
 import { Deferrable } from "@ethersproject/properties";
-import { JsonRpcProvider, JsonRpcSigner, TransactionRequest, TransactionResponse } from "@ethersproject/providers";
+import { JsonRpcSigner, TransactionRequest, TransactionResponse } from "@ethersproject/providers";
 
 import { ChainId } from "./chain";
 import { AllowListService } from "./services/allowlist";
 import { SdkError } from "./types/common";
 
 export class ValidatedJsonRpcSigner extends JsonRpcSigner {
-  private allowList: AllowListService<ChainId>;
+  allowList?: AllowListService<ChainId>;
 
-  constructor(
-    constructorGuard: any,
-    provider: JsonRpcProvider,
-    allowList: AllowListService<ChainId>,
-    addressOrIndex?: string | number
-  ) {
-    super(constructorGuard, provider, addressOrIndex);
-    this.allowList = allowList;
-  }
+  // constructor(
+  //   constructorGuard: any,
+  //   provider: JsonRpcProvider,
+  //   allowList: AllowListService<ChainId>,
+  //   addressOrIndex?: string | number
+  // ) {
+  //   super(constructorGuard, provider, addressOrIndex);
+  //   this.allowList = allowList;
+  // }
 
   async sendTransaction(transaction: Deferrable<TransactionRequest>): Promise<TransactionResponse> {
-    const valid = await this.validateTx(transaction);
-    if (!valid) {
+    if (!this.allowList) {
+      return super.sendTransaction(transaction);
+    }
+    const valid = await this.validateTx(transaction, this.allowList);
+    if (valid) {
       throw new SdkError("transaction is not valid");
     }
     return super.sendTransaction(transaction);
   }
 
-  private async validateTx(transaction: Deferrable<TransactionRequest>): Promise<boolean> {
+  private async validateTx(
+    transaction: Deferrable<TransactionRequest>,
+    allowList: AllowListService<ChainId>
+  ): Promise<boolean> {
     const [to, data] = await Promise.all([transaction.to, transaction.data]);
-    return await this.allowList.validateCalldata(to, data).then(res => res.success);
+    return await allowList.validateCalldata(to, data).then(res => res.success);
   }
 }
