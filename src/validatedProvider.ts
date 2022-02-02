ed5a10c784981d683ab9c722389f41a034b1d663
jstash <19509999+jstashh@users.noreply.github.com>
2022-02-02 17:01:32 +0000

    fix: tidy up validated provider

--- src/validatedProvider.ts
+++ src/validatedProvider.ts
@@ -4,34 +4,24 @@ import { JsonRpcProvider, JsonRpcSigner, TransactionRequest } from "@ethersproje
 import { ChainId } from "./chain";
 import { AllowListService } from "./services/allowlist";
 import { SdkError } from "./types/common";
-// import { ValidatedJsonRpcSigner } from "./validatedSigner";
 
 const originalJsonRpcSignerSendTransaction = JsonRpcSigner.prototype.sendTransaction;
 
 export class ValidatedJsonRpcProvider extends JsonRpcProvider {
   allowList?: AllowListService<ChainId>;
 
-  // constructor(url: string, network: Networkish, allowList: AllowListService<ChainId>) {
-  //   super(url, network);
-  //   this.allowList = allowList;
-  // }
-
   getSigner(addressOrIndex?: string | number): JsonRpcSigner {
-    if (this.allowList) {
-      const signer = super.getSigner(addressOrIndex);
-      signer.sendTransaction = async (transaction: Deferrable<TransactionRequest>) => {
-        if (this.allowList) {
-          const valid = await this.validateTx(transaction, this.allowList);
-          if (valid) {
-            throw new SdkError("transaction is not valid");
-          }
+    const signer = super.getSigner(addressOrIndex);
+    signer.sendTransaction = async (transaction: Deferrable<TransactionRequest>) => {
+      if (this.allowList) {
+        const valid = await this.validateTx(transaction, this.allowList);
+        if (!valid) {
+          throw new SdkError("transaction is not valid");
         }
-        return originalJsonRpcSignerSendTransaction.apply(signer, [transaction]);
-      };
-      return signer;
-    } else {
-      return super.getSigner(addressOrIndex);
-    }
+      }
+      return originalJsonRpcSignerSendTransaction.apply(signer, [transaction]);
+    };
+    return signer;
   }
 
   private validateTx = async (transaction: Deferrable<TransactionRequest>, allowList: AllowListService<ChainId>) => {
