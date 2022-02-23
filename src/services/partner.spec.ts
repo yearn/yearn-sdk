3dc9ff7a715a580e7684505cecc077d1a22efbf6
FiboApe <fiboape@gmail.com>
2022-02-23 17:55:41 -0300

    test: add tests for partner servi e

---
+++ src/services/partner.spec.ts
@@ -0,0 +1,59 @@
+import { Context } from "..";
+import { PartnerService } from "./partner";
+
+jest.mock("../context", () => ({
+  Context: jest.fn().mockImplementation(() => ({
+    events: {
+      on: jest.fn()
+    },
+    provider: {},
+    partnerId: "partnerid"
+  }))
+}));
+
+// jest.mock("@ethersproject/contracts", () => ({
+//   Contract: jest.fn().mockImplementation(() => ({
+//     deposit: jest.fn(),
+//     withdraw: jest.fn()
+//   }))
+// }));
+
+describe("VaultInterface", () => {
+  let partner: PartnerService<1>;
+
+  beforeEach(() => {
+    partner = new PartnerService(1, new Context({}), "0x8ee392a4787397126c163cb9844d7c447da419d8");
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("deposit", () => {
+    it("should call the contract deposit", () => {
+      // got to work around the fact that write is read-only
+      Object.defineProperty(partner.contract, "write", {
+        value: { deposit: jest.fn() },
+        configurable: true,
+        writable: true
+      });
+      const spy = jest.spyOn(partner.contract.write, "deposit");
+
+      partner.deposit("vault", "0.11", { from: "0x0001" });
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith("vault", "partnerid", "0.11", { from: "0x0001" });
+    });
+  });
+
+  describe("encodeDeposit", () => {
+    it("should call the contract deposit", () => {
+      const spy = jest.spyOn(partner.contract.write.interface, "encodeFunctionData").mockImplementation();
+
+      partner.encodeDeposit("vault", "0.11");
+
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledWith("deposit", ["vault", "partnerid", "0.11"]);
+    });
+  });
+});
