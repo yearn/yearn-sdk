a4dec99c4f35bc0af3e85e341ab10cd7d918dcbd
mariuspod <14898268+mariuspod@users.noreply.github.com>
2022-03-15 02:21:04 +0100

    fix: pass the addresses through so the lens adapter will be called with the correct params.

--- src/interfaces/vault.ts
+++ src/interfaces/vault.ts
@@ -61,7 +61,7 @@ export class VaultInterface<T extends ChainId> extends ServiceInterface<T> {
 
     let assetsDynamic: VaultDynamic[] = [];
     try {
-      assetsDynamic = await this.getDynamic();
+      assetsDynamic = await this.getDynamic(addresses);
     } catch {
       const allAddresses = assetsStatic.map(asset => asset.address);
       const chunks = chunkArray(allAddresses, 30);
