1ee989b3e05c5718372266e95d13d16661097d66
Zannis Kalampoukis <zannis@users.noreply.github.com>
2022-03-22 17:55:13 +0200

    fix: Mark LensService as deprecated and move adapters to Yearn object (#252)
    * fix: Mark LensService as deprecated and move adapters to Yearn object
    
    * Add another warning

--- src/services/lens.ts
+++ src/services/lens.ts
@@ -5,25 +5,19 @@ import { ContractService, WrappedContract } from "../common";
 import { ContractAddressId } from "../common";
 import { structArray } from "../struct";
 import { Address, GenericAsset, Position } from "../types";
+import { Adapters } from "../yearn";
 import { IronBankAdapter } from "./adapters/ironbank";
-import { RegistryAdapter, RegistryV2Adapter } from "./adapters/registry";
+import { RegistryV2Adapter } from "./adapters/registry";
 
 export const LensAbi = ["function getRegistries() external view returns (address[] memory)"];
-
-export type Adapters<T extends ChainId> = {
-  vaults: {
-    v1: RegistryAdapter;
-    v2: RegistryV2Adapter<T>;
-  };
-  ironBank: IronBankAdapter<T>;
-};
 
 /**
  * [[LensService]] provides access to all yearn's assets and user positions.
  * It's implemented in the form of a contract that lives on all networks
  * supported by yearn.
  *
- * WARNING: This service will be deprecated soon
+ * @deprecated since v1.2.2. Adapters accessed via [[LensService]] should
+ * now be accessed directly from the Yearn SDK object.
  */
 export class LensService<T extends ChainId> extends ContractService<T> {
   static abi = LensAbi;
@@ -31,6 +25,7 @@ export class LensService<T extends ChainId> extends ContractService<T> {
   static contractId = ContractAddressId.unused;
 
   get adapters(): Adapters<T> {
+    console.warn("Using method in deprecated service. Please use the adapters object in the Yearn SDK object instead.");
     return {
       vaults: {
         v2: new RegistryV2Adapter(this.chainId, this.ctx, this.addressProvider)
@@ -40,11 +35,13 @@ export class LensService<T extends ChainId> extends ContractService<T> {
   }
 
   get contract(): Promise<WrappedContract> {
+    console.warn("Fetching a deprecated contract. This contract address does not exist onchain anymore.");
     return this._getContract(LensService.abi, LensService.contractId, this.ctx);
   }
 
   /**
    * Get all the adapter addresses attached to Lens.
+   * @deprecated since v1.2.2
    * @param overrides
    * @returns list of registry addresses
    */
@@ -55,6 +52,7 @@ export class LensService<T extends ChainId> extends ContractService<T> {
 
   /**
    * Get all the assets from all the adapters attached to Lens.
+   * @deprecated since v1.2.2
    * @param overrides
    * @returns list of assets
    */
@@ -66,6 +64,7 @@ export class LensService<T extends ChainId> extends ContractService<T> {
   /**
    * Get all positions of a particular assets for all the assets in all the
    * adapters attached to lens.
+   * @deprecated since v1.2.2
    * @param address
    * @param overrides
    * @returns list of user positions
@@ -77,6 +76,7 @@ export class LensService<T extends ChainId> extends ContractService<T> {
 
   /**
    * Get all the assets from a specific Lens adapter.
+   * @deprecated since v1.2.2
    * @param adapter
    * @param overrides
    * @returns list of assets
--- src/yearn.ts
+++ src/yearn.ts
@@ -7,6 +7,8 @@ import { SimulationInterface } from "./interfaces/simulation";
 import { StrategyInterface } from "./interfaces/strategy";
 import { TokenInterface } from "./interfaces/token";
 import { VaultInterface } from "./interfaces/vault";
+import { IronBankAdapter } from "./services/adapters/ironbank";
+import { IRegistryAdapter, RegistryV2Adapter } from "./services/adapters/registry";
 import { AddressProvider } from "./services/addressProvider";
 import { AllowListService } from "./services/allowlist";
 import { AssetService } from "./services/assets";
@@ -22,6 +24,14 @@ import { TransactionService } from "./services/transaction";
 import { VisionService } from "./services/vision";
 import { ZapperService } from "./services/zapper";
 import { AssetServiceState } from "./types";
+
+export type Adapters<T extends ChainId> = {
+  vaults: {
+    v1: IRegistryAdapter;
+    v2: RegistryV2Adapter<T>;
+  };
+  ironBank: IronBankAdapter<T>;
+};
 
 type ServicesType<T extends ChainId> = {
   lens: LensService<T>;
@@ -57,6 +67,7 @@ export class Yearn<T extends ChainId> {
   _ctxValue: ContextValue;
 
   services: ServicesType<T>;
+  adapters: Adapters<T>;
   vaults: VaultInterface<T>;
   tokens: TokenInterface<T>;
   earnings: EarningsInterface<T>;
@@ -100,6 +111,8 @@ export class Yearn<T extends ChainId> {
       assetServiceState
     );
 
+    this.adapters = this._initAdapters(chainId);
+
     this.vaults = new VaultInterface(this, chainId, this.context);
     this.tokens = new TokenInterface(this, chainId, this.context);
     this.earnings = new EarningsInterface(this, chainId, this.context);
@@ -116,6 +129,7 @@ export class Yearn<T extends ChainId> {
     const allowListService = new AllowListService(chainId, this.context, this.addressProvider);
 
     this.services = this._initServices(chainId, this.context, this.addressProvider, allowListService);
+    this.adapters = this._initAdapters(chainId);
 
     this.vaults = new VaultInterface(this, chainId, this.context);
     this.tokens = new TokenInterface(this, chainId, this.context);
@@ -150,5 +164,14 @@ export class Yearn<T extends ChainId> {
       transaction: new TransactionService(chainId, ctx, allowlistService),
       partner: ctx.partnerId ? new PartnerService(chainId, ctx, addressProvider, ctx.partnerId) : undefined
     };
+  }
+
+  _initAdapters<T extends ChainId>(chainId: ChainId) {
+    return {
+      vaults: {
+        v2: new RegistryV2Adapter(chainId, this.context, this.addressProvider)
+      },
+      ironBank: new IronBankAdapter(chainId, this.context, this.addressProvider)
+    } as Adapters<T>;
   }
 }
