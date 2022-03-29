01501262ee9d9c6f58fdc628d6c909fa4f0f517e
Karelian Pie <devkarelianpie@gmail.com>
2022-03-30 09:58:34 +1100

    chore: Add clarifying comments to token balance/price

--- src/test-utils/factories/balance.factory.ts
+++ src/test-utils/factories/balance.factory.ts
@@ -4,9 +4,9 @@ import { createMockToken } from "./token.factory";
 const DEFAULT_BALANCE: Balance = {
   address: "0x000",
   token: createMockToken(),
-  priceUsdc: "1000000",
-  balance: "1000000000000000000",
-  balanceUsdc: "1000000"
+  priceUsdc: "1000000", // $1 (Decimal: 6)
+  balance: "1000000000000000000", // 1 (Decimal: 18)
+  balanceUsdc: "1000000" // $1 (Decimal: 6)
 };
 
 export const createMockBalance = (overwrites: Partial<Balance> = {}): Balance => ({
--- src/test-utils/factories/tokenBalance.factory.ts
+++ src/test-utils/factories/tokenBalance.factory.ts
@@ -2,9 +2,9 @@ import { TokenBalance } from "../..";
 
 const DEFAULT_TOKEN_BALANCE: TokenBalance = {
   address: "0x001",
-  priceUsdc: "1000000",
-  balance: "1000000000000000000",
-  balanceUsdc: "1000000"
+  priceUsdc: "1000000", // $1 (Decimal: 6)
+  balance: "1000000000000000000", // 1 (Decimal: 18)
+  balanceUsdc: "1000000" // $1 (Decimal: 6)
 };
 
 export const createMockTokenBalance = (overwrites: Partial<TokenBalance> = {}): TokenBalance => ({
