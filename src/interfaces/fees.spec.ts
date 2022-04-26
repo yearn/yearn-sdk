b9ab5458112e8825493eccc9af8558558471d0ad
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-04-26 22:20:10 +0800

    test: Add test for FeesInterface

---
+++ src/interfaces/fees.spec.ts
@@ -0,0 +1,94 @@
+import BigNumber from "bignumber.js";
+
+import { ApyMap, ChainId, FeesInterface, Usdc, VaultStatic } from "..";
+import { Context } from "../context";
+import { Yearn } from "../yearn";
+
+const subgraphFetchQueryMock = jest.fn();
+const visionApyMock: jest.Mock<Promise<ApyMap<string>>> = jest.fn();
+const lensAdaptersVaultsV2AssetsStaticMock: jest.Mock<Promise<VaultStatic[]>> = jest.fn();
+const oracleGetPriceUsdcMock: jest.Mock<Promise<Usdc>> = jest.fn();
+const getBlockNumberMock: jest.Mock<Promise<number>> = jest.fn();
+
+const dateMock = new Date("2022-01-01");
+jest.spyOn(global, "Date").mockImplementation(() => dateMock as unknown as string);
+
+jest.mock("../yearn", () => ({
+  Yearn: jest.fn().mockImplementation(() => ({
+    services: {
+      subgraph: {
+        fetchQuery: subgraphFetchQueryMock,
+      },
+      vision: {
+        apy: visionApyMock,
+      },
+      lens: {
+        adapters: { vaults: { v2: { assetsStatic: lensAdaptersVaultsV2AssetsStaticMock } } },
+      },
+      oracle: {
+        getPriceUsdc: oracleGetPriceUsdcMock,
+      },
+    },
+  })),
+}));
+
+jest.mock("../context", () => ({
+  Context: jest.fn().mockImplementation(() => ({
+    provider: {
+      read: {
+        getBlockNumber: getBlockNumberMock,
+      },
+    },
+  })),
+}));
+
+describe("FeesInterface", () => {
+  let feesInterface: FeesInterface<1>;
+  let mockedYearn: Yearn<ChainId>;
+
+  beforeEach(() => {
+    mockedYearn = new (Yearn as jest.Mock<Yearn<ChainId>>)();
+    feesInterface = new FeesInterface(mockedYearn, 1, new Context({}));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("protocolFees", () => {
+    describe("when there is no data", () => {
+      beforeEach(() => {
+        subgraphFetchQueryMock.mockResolvedValue(undefined);
+      });
+
+      it("should return a string representing the value of BigNumber(0)", async () => {
+        const actualProtocolFees = await feesInterface.protocolFees(new Date());
+
+        expect(actualProtocolFees).toEqual("0");
+      });
+    });
+
+    describe("when there is data", () => {
+      beforeEach(() => {
+        subgraphFetchQueryMock.mockResolvedValue({
+          data: {
+            transfers: [
+              {
+                tokenAmountUsdc: new BigNumber(3),
+              },
+              {
+                tokenAmountUsdc: new BigNumber(4),
+              },
+            ],
+          },
+        });
+      });
+
+      it("should return the protocol fees", async () => {
+        const actualProtocolFees = await feesInterface.protocolFees(new Date());
+
+        expect(actualProtocolFees).toEqual(new BigNumber(7).toFixed(0));
+      });
+    });
+  });
+});
--- src/interfaces/fees.ts
+++ src/interfaces/fees.ts
@@ -6,18 +6,26 @@ import { PROTOCOL_FEES } from "../services/subgraph/queries";
 import { FeesResponse } from "../services/subgraph/responses";
 import { Usdc } from "../types";
 
+const BigZero = new BigNumber(0);
+
 export class FeesInterface<C extends ChainId> extends ServiceInterface<C> {
   async protocolFees(since: Date): Promise<Usdc> {
     const response = await this.yearn.services.subgraph.fetchQuery<FeesResponse>(PROTOCOL_FEES, {
       sinceDate: since.getTime().toString(),
     });
 
+    let result = BigZero;
+
+    if (!response?.data || !response.data?.transfers) {
+      return result.toFixed(0);
+    }
+
     const transfers = response.data.transfers;
 
     return transfers
       .reduce((prev, current) => {
         return prev.plus(new BigNumber(current.tokenAmountUsdc || 0));
-      }, new BigNumber(0))
+      }, result)
       .toFixed(0);
   }
 }
