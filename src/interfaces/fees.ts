e3fe516736cccca0c67520768bf9fed816d8ee34
huggingbot <83656073+huggingbot@users.noreply.github.com>
2022-04-29 00:41:10 +0800

    test: Add test for `FeesInterface` (#285)
    * test: Add test for FeesInterface
    
    * fix: Apply suggested changes
    
    * fix: Apply suggested changes

--- src/interfaces/fees.ts
+++ src/interfaces/fees.ts
@@ -1,10 +1,9 @@
-import BigNumber from "bignumber.js";
-
 import { ChainId } from "../chain";
 import { ServiceInterface } from "../common";
 import { PROTOCOL_FEES } from "../services/subgraph/queries";
 import { FeesResponse } from "../services/subgraph/responses";
 import { Usdc } from "../types";
+import { toBN } from "../utils";
 
 export class FeesInterface<C extends ChainId> extends ServiceInterface<C> {
   async protocolFees(since: Date): Promise<Usdc> {
@@ -12,12 +11,12 @@ export class FeesInterface<C extends ChainId> extends ServiceInterface<C> {
       sinceDate: since.getTime().toString(),
     });
 
+    if (!response?.data || !response.data?.transfers) {
+      return toBN(0).toFixed(0);
+    }
+
     const transfers = response.data.transfers;
 
-    return transfers
-      .reduce((prev, current) => {
-        return prev.plus(new BigNumber(current.tokenAmountUsdc || 0));
-      }, new BigNumber(0))
-      .toFixed(0);
+    return transfers.reduce((prev, { tokenAmountUsdc }) => prev.plus(toBN(tokenAmountUsdc || 0)), toBN(0)).toFixed(0);
   }
 }
