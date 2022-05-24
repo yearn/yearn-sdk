9258feb8074c5c0550ba6a0432fba806a7faf745
jstash <19509999+jstashh@users.noreply.github.com>
2022-05-24 17:32:07 +0100

    fix: undo debug

--- examples/fantom/vault.mjs
+++ examples/fantom/vault.mjs
@@ -13,14 +13,12 @@ const yearn = new Yearn(250, {
 });
 
 async function main() {
-  const result = await yearn.strategies.getHarvests("0xA9a904B5567b5AFfb6bB334bea2f90F700EB221a");
-  console.log(result);
-  // const vaults = await yearn.vaults.getDynamic();
-  // console.log(vaults);
-  // const tokenMetadata = await yearn.services.meta.token("0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83");
-  // console.log(tokenMetadata);
-  // const vaultMetadata = await yearn.services.meta.vaults("0x1DF3f4985Ac2103B96ed75f9d76F33696D2b1d66");
-  // console.log(vaultMetadata);
+  const vaults = await yearn.vaults.getDynamic();
+  console.log(vaults);
+  const tokenMetadata = await yearn.services.meta.token("0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83");
+  console.log(tokenMetadata);
+  const vaultMetadata = await yearn.services.meta.vaults("0x1DF3f4985Ac2103B96ed75f9d76F33696D2b1d66");
+  console.log(vaultMetadata);
 }
 
 main();
