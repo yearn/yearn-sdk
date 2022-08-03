c7acd42724e34f2e6e175e9124c0b7865153eddc
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-08-02 20:27:04 -0600

    refactor: simplify vault deposit and withdraw

--- src/services/transaction.ts
+++ src/services/transaction.ts
@@ -17,17 +17,30 @@ export class TransactionService<T extends ChainId> extends Service {
 
   async sendTransaction(transaction: Deferrable<TransactionRequest>): Promise<TransactionResponse> {
     const { success, error } = await this.validateTx(transaction);
-    if (!success) {
-      throw new SdkError(error || "transaction is not valid");
-    }
+    if (!success) throw new SdkError(error || "transaction is not valid");
+
     const signer = this.ctx.provider.write.getSigner();
-    return signer.sendTransaction(transaction);
+    try {
+      return signer.sendTransaction(transaction);
+    } catch (error) {
+      if ((error as { code: number }).code === -32602) {
+        const txPayload = { ...transaction };
+        txPayload.maxFeePerGas = undefined;
+        txPayload.maxPriorityFeePerGas = undefined;
+        return await signer.sendTransaction(txPayload);
+      }
+
+      throw error;
+    }
+  }
+
+  async populateTransaction(transaction: Deferrable<TransactionRequest>): Promise<TransactionRequest> {
+    const signer = this.ctx.provider.write.getSigner();
+    return await signer.populateTransaction(transaction);
   }
 
   private async validateTx(transaction: Deferrable<TransactionRequest>): Promise<{ success: boolean; error?: string }> {
-    if (!this.allowListService) {
-      return Promise.resolve({ success: true, error: undefined });
-    }
+    if (!this.allowListService) return Promise.resolve({ success: true, error: undefined });
 
     const [to, data] = await Promise.all([transaction.to, transaction.data]);
     return await this.allowListService.validateCalldata(to, data);
