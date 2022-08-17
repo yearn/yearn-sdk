bdd27eff40009a50bcc14bf3d28ce51afd15355d
Karelian Pie <devkarelianpie@gmail.com>
2022-08-17 17:33:37 +1000

    fix: Cannot read properties of undefined

--- src/utils/localization.spec.ts
+++ src/utils/localization.spec.ts
@@ -2,16 +2,19 @@ import { getLocalizedString } from "./localization";
 
 describe("Localization", () => {
   describe("getLocalizedString", () => {
-    it("should throw TypeError when localization is `undefined`", () => {
-      expect(() =>
-        getLocalizedString({
-          obj: {
-            localization: undefined as unknown as string,
-          },
-          locale: "en",
-          fallback: "foobar",
-        })
-      ).toThrowError(new TypeError("Cannot read properties of undefined (reading 'en')"));
+    it("should not throw TypeError when localization is `undefined`", () => {
+      let actual;
+      expect(
+        () =>
+          (actual = getLocalizedString({
+            obj: {
+              localization: undefined as unknown as string,
+            },
+            locale: "en",
+            fallback: "foobar",
+          }))
+      ).not.toThrowError(new TypeError("Cannot read properties of undefined (reading 'en')"));
+      expect(actual).toEqual("foobar");
     });
   });
 });
