5c4563222c5bc3b13836f55b206f6e3fa35b749a
Sero <69639595+Seroxdesign@users.noreply.github.com>
2022-10-08 13:18:48 -0400

    supporting goerli

--- src/chain.spec.ts
+++ src/chain.spec.ts
@@ -1,22 +1,24 @@
-import { allSupportedChains, isArbitrum, isEthereum, isFantom } from "./chain";
+import { allSupportedChains, isArbitrum, isEthereum, isFantom, isGoerli } from "./chain";
 
 describe("Chain", () => {
   it.each`
-    chainId  | ethereum | fantom   | arbitrum
-    ${1}     | ${true}  | ${false} | ${false}
-    ${250}   | ${false} | ${true}  | ${false}
-    ${1337}  | ${true}  | ${false} | ${false}
-    ${42161} | ${false} | ${false} | ${true}
-    ${42}    | ${false} | ${false} | ${false}
-  `(`should return the correct result for each chain "$chainId"`, ({ chainId, ethereum, fantom, arbitrum }) => {
+    chainId  | ethereum | fantom   | arbitrum | goerli
+    ${1}     | ${true}  | ${false} | ${false} | ${false}
+    ${5}     | ${false} | ${false} | ${false} | ${true}
+    ${250}   | ${false} | ${true}  | ${false} | ${false}
+    ${1337}  | ${true}  | ${false} | ${false} | ${false}
+    ${42161} | ${false} | ${false} | ${true}  | ${false}
+    ${42}    | ${false} | ${false} | ${false} | ${false}
+  `(`should return the correct result for each chain "$chainId"`, ({ chainId, ethereum, fantom, arbitrum, goerli }) => {
     expect(isEthereum(chainId)).toBe(ethereum);
+    expect(isGoerli(chainId)).toBe(goerli);
     expect(isFantom(chainId)).toBe(fantom);
     expect(isArbitrum(chainId)).toBe(arbitrum);
   });
 
   describe("allSupportedChains", () => {
     it("should return all supported chains", () => {
-      expect(allSupportedChains).toStrictEqual([1, 10, 250, 1337, 42161]);
+      expect(allSupportedChains).toStrictEqual([1, 5, 10, 250, 1337, 42161]);
     });
   });
 });
